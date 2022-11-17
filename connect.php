23bd7723fcc7737a1370cb3c60cbd28d28875ef8
Khaliidgh <111587860+Khaliidgh@users.noreply.github.com>
2022-11-17 14:35:47 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-$conn = pg_connect("host=192.168.40.132 port=5432  user=caux password=caux dbname=caux");
+$conn = pg_connect("host=192.168.56.145 port=5432  user=caux password=caux dbname=caux");
 	if (!$conn) {
 		die("database connection failed" . pg_last_error($conn));
 	}
